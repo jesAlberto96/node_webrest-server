ab798915a491f9140dfc4470f4bdfd420b292748
Jesus Alberto Garcia <jes.alberto96@gmail.com>
2024-08-16 18:46:09 -0500

    Todo con conexion a base de datos usando prisma

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,10 +1,6 @@
 import { Request, Response } from "express";
-
-const todos = [
-    {id: 1, text: 'Buy milk', createdAt: new Date()},
-    {id: 2, text: 'Buy bread', createdAt: null},
-    {id: 3, text: 'Buy Butter', createdAt: new Date()},
-];
+import { prisma } from "../../data/postgres";
+import { CreateTodoDto, UpdateTodoDto } from "../../domain/dtos";
 
 export class TodosController {
 
@@ -12,58 +8,69 @@ export class TodosController {
 
     }
 
-    public getTodos = (req: Request, res: Response) => {
+    public getTodos = async (req: Request, res: Response) => {
+        const todos = await prisma.todo.findMany();
         return res.json(todos);
     }
 
-    public getTodoById = (req: Request, res: Response) => {
+    public getTodoById = async (req: Request, res: Response) => {
         const id = +req.params.id;
 
         if (isNaN(id)) return res.status(400).json({error: 'ID argument is not a number'})
 
-        const todo = todos.find(todo => todo.id === id);
+        const todo = await prisma.todo.findFirst({where: { id } });
 
         (todo) ? res.json(todo) : res.status(404).json({ error: `TODO with id ${id} not found` });
     }
 
-    public createTodo = (req: Request, res: Response) => {
-        const { text } = req.body;
-        if (!text) return res.status(400).json({error: 'Text property is required'});
-        const newTodo = {
-            id: todos.length + 1,
-            text,
-            createdAt: new Date()
-        };
-        todos.push(newTodo);
-        res.json(newTodo);
-    }
+    public createTodo = async (req: Request, res: Response) => {
+        const [error, createTodoDto] = CreateTodoDto.create(req.body);
+        if (error) return res.status(400).json({error})
 
-    public updateTodo = (req: Request, res: Response) => {
-        const id = +req.params.id;
-
-        if (isNaN(id)) return res.status(400).json({error: 'ID argument is not a number'})
-
-        const todo = todos.find(todo => todo.id === id);
-        if (!todo) return res.status(404).json({ error: `TODO with id ${id} not found` });
-
-        const { text, createdAt } = req.body;
-        if (!text) return res.status(400).json({error: 'Text property is required'});
-
-        todo.text = text || todo.text;
-        (createdAt === 'null') ? todo.createdAt = null : todo.createdAt = new Date(createdAt || todo.createdAt);
+        const todo = await prisma.todo.create({
+            data: createTodoDto!
+        });
 
         res.json(todo);
     }
 
-    public deleteTodo = (req: Request, res: Response) => {
+    public updateTodo = async (req: Request, res: Response) => {
+        const id = +req.params.id;
+        const [error, updateTodoDto] = UpdateTodoDto.create({
+            ...req.body, id
+        });
+
+        if (error) return res.status(400).json({error})
+
+        const todo = await prisma.todo.findFirst({where: { id } });
+        if (!todo) return res.status(404).json({ error: `TODO with id ${id} not found` });
+
+        const updatedTodo = await prisma.todo.update({
+            where: { id },
+            data: updateTodoDto!.values
+        });
+
+        res.json(updatedTodo);
+    }
+
+    public deleteTodo = async (req: Request, res: Response) => {
         const id = +req.params.id;
 
         if (isNaN(id)) return res.status(400).json({error: 'ID argument is not a number'})
 
-        const todo = todos.find(todo => todo.id === id);
+        const todo = await prisma.todo.findFirst({
+            where: {
+                id,
+            }
+        });
         if (!todo) return res.status(404).json({ error: `TODO with id ${id} not found` });
 
-        todos.slice(todos.indexOf(todo), 1);
-        res.json(todo);
+        const deleted = await prisma.todo.delete({
+            where: {
+                id
+            }
+        });
+
+        res.json({deleted});
     }
 }
